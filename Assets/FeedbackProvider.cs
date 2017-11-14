d139af4fab6a45dd77f2a309be5a907df3aa7231
Jonathan <jonathanraes@gmail.com>
2017-11-14 17:03:56 +0100

    2D scene that draws circle on mouse press

---
+++ Assets/FeedbackProvider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedbackProvider : MonoBehaviour {
+
+	public GameObject feedback;
+	public UDPReceive udpreceiver;
+
+	private GameObject activeFeedback;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetMouseButtonDown (0)) {
+			if (activeFeedback != null) Destroy(activeFeedback);
+			
+			Vector3 pos = Input.mousePosition;
+			Debug.Log (pos);
+			pos = Camera.main.ScreenToWorldPoint (pos);
+			pos.z = 0;
+			activeFeedback = Instantiate (feedback, pos, Quaternion.identity);
+		}
+	}
+
+}
