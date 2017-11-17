0e1c95f1def99c549ddb674db34265f6ae97ad05
Jonathan <jonathanraes@gmail.com>
2017-11-17 16:55:11 +0100

    added NFC scanner highlighter and suport for different messages over udp

--- Assets/Scripts/Hightlighter.cs
+++ Assets/Scripts/Hightlighter.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class Hightlighter : MonoBehaviour {
 
-	public GameObject highlight; 
+	public GameObject highlight;
+	public Vector2 NFCLocation;
+	public GameObject SelectButton;
+	public GameObject LeftButton;
+	public GameObject RightButton;
 
+	private bool NFCHighlighenabled = false;
+	private GameObject spawnedHighlight;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,10 +19,29 @@ public class Hightlighter : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		switch (CurrentState.currentState) {
+		case State.WAIT_FOR_NFC:
+			enableNFCHighlight ();
+			break;
+		case State.SELECTING:
+			disableNFCHighlight ();
+			break;
+		default:
+			break;
+		}
 	}
 
-	public void enableHighlight() {
+	public void enableNFCHighlight() {
+		if (!NFCHighlighenabled) {
+			spawnedHighlight= Instantiate (highlight, NFCLocation, Quaternion.identity);
+			NFCHighlighenabled = true;
+		}
+	}
 
+	public void disableNFCHighlight() {
+		if (NFCHighlighenabled) {
+			Destroy (spawnedHighlight);
+			NFCHighlighenabled = false;
+		}
 	}
 }
--- Assets/Scripts/TouchInterpreter.cs
+++ Assets/Scripts/TouchInterpreter.cs
@@ -7,7 +7,7 @@ using SimpleJSON;
 public class TouchInterpreter : MonoBehaviour {
 
 	public UDPReceive receiver;
-	List<Vector2> myList = new List<Vector2>();
+	List<Vector2> touchPoints = new List<Vector2>();
 
 	// Use this for initialization
 	void Start () {
@@ -17,40 +17,30 @@ public class TouchInterpreter : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (receiver != null) {
-			string packet = receiver.getLatestUDPPacket ();
-			if (packet != null) {
-
-				myList.Clear ();
-				var N = JSON.Parse(packet);
-				//Debug.Log (N ["cursors"].AsArray [0].AsArray);
-				if (N != null) {
-					JSONArray points = N ["cursors"].AsArray;
-					foreach (object p in points) {
-						JSONNode point = JSON.Parse (p.ToString ());
-						//	Debug.Log (point.AsArray[0].AsFloat + " " + point.AsArray[1].AsFloat);
-						myList.Add (new Vector2 (point.AsArray [0].AsFloat, point.AsArray [1].AsFloat));
-					}
+			touchPoints.Clear ();
+			JSONArray points = receiver.getLastCursors ();
+			if (points != null) {
+				foreach (object p in points) {
+					JSONNode point = JSON.Parse (p.ToString ());
+					//	Debug.Log (point.AsArray[0].AsFloat + " " + point.AsArray[1].AsFloat);
+					touchPoints.Add (new Vector2 (point.AsArray [0].AsFloat, point.AsArray [1].AsFloat));
 				}
-			//	foreach (var point in N["cursors"].AsArray[0].AsArray) {
-			//		Debug.Log (point[0] + "," + point[1]);
-			//	}
-				//Debug.Log ("parsed " + N["cursors"].AsArray[0].AsArray[0] + "" + N["cursors"].AsArray[0].AsArray[0]);
 			}
 		}
 	}
 
 	public List<Vector2> getTouchPoints() {
-		return myList;
+		return touchPoints;
 	}
 
 	public Vector2 getAverageTouchPoint() {
 		float sumX = 0;
 		float sumY = 0;
-		foreach (Vector2 p in myList) {
+		foreach (Vector2 p in touchPoints) {
 			sumX += p.x;
 			sumY += p.y;
 		}
-		Vector2 result = new Vector2 (sumX / myList.Count, sumY / myList.Count);
+		Vector2 result = new Vector2 (sumX / touchPoints.Count, sumY / touchPoints.Count);
 		Debug.Log ("Average hit is: [" + result.x + ", " + result.y + "]");
 		return result;
 	}
