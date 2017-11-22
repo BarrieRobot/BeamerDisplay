8ba33001cb2bef71c57ded48774459808f26f0ed
Jonathan <jonathanraes@gmail.com>
2017-11-22 13:13:18 +0100

    read touch bounds of buttons from config file
    this way we can easily use the same settings for both scenes

--- Assets/Scripts/TouchInterpreter.cs
+++ Assets/Scripts/TouchInterpreter.cs
@@ -3,17 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Collections;
 using SimpleJSON;
+using System.IO;
 
 public class TouchInterpreter : MonoBehaviour {
 
 	public UDPReceive receiver;
 	List<Vector2> touchPoints = new List<Vector2>();
+	public string configPath = "/home/jonathan/git/BarrieBeamerDisplay/Assets/Resources/test.txt";
+
+	public Vector2 leftButtonBoundsMin;
+	public Vector2 leftButtonBoundsMax;
+
+	public Vector2 rightButtonBoundsMin;
+	public Vector2 rightButtonBoundsMax;
+
+	public Vector2 selectButtonBoundsMin;
+	public Vector2 selectButtonBoundsMax;
 
 	// Use this for initialization
 	void Start () {
-		
+
+		//Read the text from directly from the test.txt file
+		ReadConfig();
 	}
-	
+
+	void ReadConfig() {
+		StreamReader reader = new StreamReader(configPath); 
+		string line;
+		while ((line = reader.ReadLine()) != null) {
+			string[] items = line.Split('=');
+			string[] points = items [1].Split (':');
+			string[] min = points [0].Split (',');
+			string[] max = points [1].Split (',');
+			switch (items [0]) {
+			case "LeftButton":
+				leftButtonBoundsMin = new Vector2 (float.Parse (min [0]), float.Parse (min [1]));
+				leftButtonBoundsMax = new Vector2 (float.Parse (max [0]), float.Parse (max [1]));
+				break;
+			case "RightButton":
+				rightButtonBoundsMin = new Vector2 (float.Parse (min [0]), float.Parse (min [1]));
+				rightButtonBoundsMax = new Vector2 (float.Parse (max [0]), float.Parse (max [1]));
+				break;
+			case "SelectButton":
+				selectButtonBoundsMin = new Vector2 (float.Parse (min [0]), float.Parse (min [1]));
+				selectButtonBoundsMax = new Vector2 (float.Parse (max [0]), float.Parse (max [1]));
+				break;
+			default:
+				break;
+			}
+
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
 		touchPoints.Clear ();
