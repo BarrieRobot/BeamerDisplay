ce120dc7412ce92984315b6630916087a1d32429
Jonathan <jonathanraes@gmail.com>
2017-11-27 13:22:56 +0100

    add models + organize scripts

---
+++ Assets/Scripts/HologramHighlighter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HologramHighlighter : MonoBehaviour {
+
+	public GameObject scanTagPanel;
+	public GameObject chooseDrinkpanel;
+
+	public GameObject coffee;
+	public GameObject soda;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (CurrentState.currentState) {
+		case State.WAIT_FOR_NFC:
+			scanTagPanel.SetActive (true);
+			chooseDrinkpanel.SetActive (false);
+			break;
+		case State.CHOOSING_CATEGORY:
+			scanTagPanel.SetActive (false);
+			chooseDrinkpanel.SetActive (true);
+			break;
+		case State.SELECTING:
+			scanTagPanel.SetActive (false);
+			chooseDrinkpanel.SetActive (false);
+			break;
+		default:
+			break;
+		}
+	}
+}
