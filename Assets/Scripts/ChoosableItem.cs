f536bce2ed8a7c919c2c524715cf8c1602667f5f
Jonathan <jonathanraes@gmail.com>
2017-12-19 14:40:53 +0100

    add some comments and remove inline if dupes in ItemChoice

--- Assets/Scripts/ChoosableItem.cs
+++ Assets/Scripts/ChoosableItem.cs
@@ -16,25 +16,28 @@ public class ChoosableItem : MonoBehaviour
 
 	private bool gravitating = true;
 	private bool moveout = false;
-	private Direction movedir;
+	private Vector3 movedir;
 
 	void Update ()
 	{
 		if (gravitating && (gameObject.transform.localPosition.x > 1 || gameObject.transform.localPosition.x < -1)) {
+			// Item is gravitating to the centre
 			float pctFromMid = gameObject.transform.localPosition.x / SceneGlobals.sceneBound;
 			float newVel = pctFromMid * targetVel;
-			gameObject.GetComponent<Rigidbody> ().velocity = Vector3.Scale (movedir == Direction.RIGHT ? Vector3.right : Vector3.left, new Vector3 (newVel, 0, 0));
+			gameObject.GetComponent<Rigidbody> ().velocity = Vector3.Scale (movedir, new Vector3 (newVel, 0, 0));
 			//gameObject.GetComponent<Rigidbody> ().position = new Vector3 (newX, gameObject.transform.localPosition.y, gameObject.transform.localPosition.z);
 
 		} else if (gravitating) {
+			// Item has gravitated within bounds [-1, 1]
 			gameObject.GetComponent<Rigidbody> ().velocity = Vector3.zero;
 			gameObject.transform.localPosition = new Vector3 (0, gameObject.transform.localPosition.y, gameObject.transform.localPosition.z);
 			gravitating = false;
 		}
 
 		if (moveout) {
+			// Item needs to move out of the scene
 			float newVel = Mathf.Lerp (Mathf.Abs (gameObject.GetComponent<Rigidbody> ().velocity.x), targetVel, Time.deltaTime * moveOutInterpSpeed);
-			gameObject.GetComponent<Rigidbody> ().velocity = Vector3.Scale (movedir == Direction.RIGHT ? Vector3.right : Vector3.left, new Vector3 (newVel, 0, 0));
+			gameObject.GetComponent<Rigidbody> ().velocity = Vector3.Scale (movedir, new Vector3 (newVel, 0, 0));
 		}
 	}
 
@@ -45,7 +48,9 @@ public class ChoosableItem : MonoBehaviour
 
 	public void moveOut (Direction dir)
 	{
-		movedir = dir;
+		// Move this object out of the scene, sets the direction and a boolean
+		// Actual moving is done in Update 
+		movedir = dir == Direction.RIGHT ? Vector3.right : Vector3.left;
 		moveout = true;
 	}
 }
