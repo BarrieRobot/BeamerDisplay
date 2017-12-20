2d4dd8f0eb168365a075e7a8f5f5d3d65398914d
Jonathan <jonathanraes@gmail.com>
2017-12-20 16:54:21 +0100

    move items up and down in a sine to ging a feel of levitation

---
+++ Assets/Scripts/Hologram/Levitate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levitate : MonoBehaviour
+{
+
+	public float magnitude;
+	public float speed;
+
+	private float startY;
+	private bool up = true;
+
+	private float progress = 0;
+	// Use this for initialization
+	void Start ()
+	{
+		startY = gameObject.transform.localPosition.y;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		progress += Time.deltaTime;
+		float offset = Mathf.Sin (progress * speed) * magnitude;
+		gameObject.transform.localPosition = new Vector3 (gameObject.transform.localPosition.x, startY + offset, gameObject.transform.localPosition.z);
+	}
+}
