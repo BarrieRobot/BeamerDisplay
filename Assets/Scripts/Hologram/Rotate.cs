849beb187d4d472f42f7b33852635b9a9cff4238
Jonathan <jonathanraes@gmail.com>
2018-01-08 15:57:25 +0100

    updated rotation

--- Assets/Scripts/Hologram/Rotate.cs
+++ Assets/Scripts/Hologram/Rotate.cs
@@ -2,20 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotate : MonoBehaviour {
+public class Rotate : MonoBehaviour
+{
 	
-	public float RotationSpeed = 1;
+	public float WorldRotationSpeed = 1;
+	public float SelfRotationSpeed = 1;
 	public bool x, y, z;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 
 	}
 
 	// Update is called once per frame
-	void Update () {
-		gameObject.transform.Rotate(new Vector3(x ? RotationSpeed * Time.deltaTime : 0
-												, y ? RotationSpeed * Time.deltaTime : 0
-												, z ? RotationSpeed * Time.deltaTime : 0));
+	void Update ()
+	{
+		gameObject.transform.Rotate (new Vector3 (x ? WorldRotationSpeed * Time.deltaTime : 0
+			, y ? WorldRotationSpeed * Time.deltaTime : 0
+			, z ? WorldRotationSpeed * Time.deltaTime : 0), Space.World);
+		gameObject.transform.Rotate (new Vector3 (x ? SelfRotationSpeed * Time.deltaTime : 0
+			, y ? SelfRotationSpeed * Time.deltaTime : 0
+			, z ? SelfRotationSpeed * Time.deltaTime : 0), Space.Self);
 	}
 }
