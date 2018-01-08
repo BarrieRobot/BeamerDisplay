d11873f2797054d9ee66b84c50902a4e000f3174
Jonathan <jonathanraes@gmail.com>
2018-01-08 16:15:09 +0100

    remove unused code

--- Assets/Scripts/TouchInterpreter.cs
+++ Assets/Scripts/TouchInterpreter.cs
@@ -5,76 +5,26 @@ using UnityEngine.Collections;
 using SimpleJSON;
 using System.IO;
 
-public class TouchInterpreter : MonoBehaviour {
+public class TouchInterpreter : MonoBehaviour
+{
 
 	public UDPReceive receiver;
 
-	List<Vector2> touchPoints = new List<Vector2>();
-	public string configPath = "/home/jonathan/git/BarrieBeamerDisplay/Assets/Resources/test.txt";
-
-	public Vector2 leftButtonBoundsMin;
-	public Vector2 leftButtonBoundsMax;
-
-	public Vector2 rightButtonBoundsMin;
-	public Vector2 rightButtonBoundsMax;
-
-	public Vector2 selectButtonBoundsMin;
-	public Vector2 selectButtonBoundsMax;
-
-	public Vector2 HotSelectionMin;
-	public Vector2 HotSelectionMax;
-
-	public Vector2 ColdSelectionMin;
-	public Vector2 ColdSelectionMax;
+	List<Vector2> touchPoints = new List<Vector2> ();
 
 	private bool RightButtonTouched = false;
 	private bool LeftButtonTouched = false;
 	private bool SelectButtonTouched = false;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 
-		//Read the text from directly from the test.txt file
-		ReadConfig();
-	}
-
-	void ReadConfig() {
-		StreamReader reader = new StreamReader(configPath); 
-		string line;
-		while ((line = reader.ReadLine()) != null) {
-			string[] items = line.Split('=');
-			string[] points = items [1].Split (':');
-			string[] min = points [0].Split (',');
-			string[] max = points [1].Split (',');
-			switch (items [0]) {
-			case "LeftButton":
-				leftButtonBoundsMin = new Vector2 (float.Parse (min [0]), float.Parse (min [1]));
-				leftButtonBoundsMax = new Vector2 (float.Parse (max [0]), float.Parse (max [1]));
-				break;
-			case "RightButton":
-				rightButtonBoundsMin = new Vector2 (float.Parse (min [0]), float.Parse (min [1]));
-				rightButtonBoundsMax = new Vector2 (float.Parse (max [0]), float.Parse (max [1]));
-				break;
-			case "SelectButton":
-				selectButtonBoundsMin = new Vector2 (float.Parse (min [0]), float.Parse (min [1]));
-				selectButtonBoundsMax = new Vector2 (float.Parse (max [0]), float.Parse (max [1]));
-				break;
-			case "HotSelection":
-				HotSelectionMin = new Vector2 (float.Parse (min [0]), float.Parse (min [1]));
-				HotSelectionMax = new Vector2 (float.Parse (max [0]), float.Parse (max [1]));
-				break;
-			case "ColdSelection":
-				ColdSelectionMin = new Vector2 (float.Parse (min [0]), float.Parse (min [1]));
-				ColdSelectionMax = new Vector2 (float.Parse (max [0]), float.Parse (max [1]));
-				break;
-			default:
-				break;
-			}
-		}
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		touchPoints.Clear ();
 		if (receiver != null) {
 			JSONArray points = receiver.getLastCursors ();
@@ -107,11 +57,13 @@ public class TouchInterpreter : MonoBehaviour {
 		}
 	}
 
-	public List<Vector2> getTouchPoints() {
+	public List<Vector2> getTouchPoints ()
+	{
 		return touchPoints;
 	}
 
-	public Vector2 getAverageTouchPoint() {
+	public Vector2 getAverageTouchPoint ()
+	{
 		float sumX = 0;
 		float sumY = 0;
 		foreach (Vector2 p in touchPoints) {
@@ -123,20 +75,23 @@ public class TouchInterpreter : MonoBehaviour {
 		return result;
 	}
 
-	bool isTouchInBounds(Vector2 touch, Vector2 minBound, Vector2 maxBound) {
+	bool isTouchInBounds (Vector2 touch, Vector2 minBound, Vector2 maxBound)
+	{
 		if (touch.x > minBound.x && touch.x < maxBound.x
-			&& touch.y > minBound.y && touch.y < maxBound.y) {
+		    && touch.y > minBound.y && touch.y < maxBound.y) {
 			return true;
 		}
 		return false;
 	}
 
-	public Vector2 getScaledAverageTouchPoint () {
+	public Vector2 getScaledAverageTouchPoint ()
+	{
 		return scalePoint (getAverageTouchPoint ());
 	}
 
 	// Scales to [-5 ... 5]
-	public Vector2 scalePoint(Vector2 p) {
+	public Vector2 scalePoint (Vector2 p)
+	{
 		return new Vector2 ((p.x - 0.5f) * 10, p.y);
 	}
 }
